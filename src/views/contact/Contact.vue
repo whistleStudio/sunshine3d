56f74a2a1b09a90c9306b00375cb11a7d2b45301
whistleStudio <435424527@qq.com>
2022-02-06 13:08:21 +0800

    202202061308

--- src/views/contact/Contact.vue
+++ src/views/contact/Contact.vue
@@ -6,25 +6,37 @@
       <div id="cTitle">联系上善</div>
       <div id="cInfo">
         <ul>
-          <li v-for="v in infoData" :key="v.id"><div class="cIcon" :style="{backgroundImage: v.img}"></div> <span class="cIconText">{{v.text}}</span></li>
+          <li v-for="v in infoData.slice(0,2)" :key="v.id"><div class="cIcon" :style="{backgroundImage: v.img}"></div> <span class="cIconText">{{v.text}}</span></li>
+          <li>
+            <div class="cIcon" :style="{backgroundImage: infoData[2].img}"></div> 
+            <div>
+              <span class="cIconText" v-for="(v,i) in infoData[2].text" :key="i">{{v[0]}}<br>{{v[1]}}</span>
+            </div>
+          </li>
         </ul>
         <div id="cSend">
           <div>留言给我们:</div>
           <div>
-            <div><input type="text"></div>
-            <div>
-              <select name="" id="">
+            <div id="cSend1" ><input type="text" placeholder="公司"></div>
+            <div id="cSend2">
+              <select name="" id="" class="sendHalf">
                 <option disabled value="">问题分类</option>
                 <option v-for="(v,i) in ['产品咨询','技术支持', '商务合作', '其他']" :key="i">{{v}}</option>
               </select>
-              <input type="text">
+              <input type="text" placeholder="*姓名" class="sendHalf">
             </div>
-            <div><input type="text"> <input type="text"></div>
-            <div><textarea name="" id="" cols="30" rows="10"></textarea></div>
+            <div id="cSend3" style="height: 2.5vh"><input type="text" placeholder="*邮箱(我们会在24小时内回复您)" class="sendHalf"> <input type="text" placeholder="*所在地" class="sendHalf"></div>
+            <div id="cSend4"><textarea name="" id="" cols="30" rows="10" placeholder="*详情描述"></textarea></div>
+            <div id="cSubmit">提交</div>
           </div>
         </div>
       </div>
-      <div id="cOther">xxxxxxx</div>
+      <div id="cOther">
+        <div>关注我们</div>
+        <ul>
+          <li v-for="(v, i) in Array(5)" :key="i" :style="{backgroundImage: `url(${require('img/contact/o'+i+'.png')})`}"></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -40,7 +52,7 @@
         infoData: [
           {id:0, img:`url(${require("img/contactIcon2.png")})`, text:"江苏省南京市江宁区xxx街道xxx大厦"},
           {id:1, img:`url(${require("img/contactIcon0.png")})`, text:"123456789"},
-          {id:2, img:`url(${require("img/contactIcon1.png")})`, text:"1234564789@126.com"},
+          {id:2, img:`url(${require("img/contactIcon1.png")})`, text:[["(商务合作)","1234564789@126.com"],["(加入我们)","987654321@126.com"]]},
         ],
       };
     },
@@ -50,78 +62,6 @@
   }
 </script>
 
+<style src="views/contact/contact.css" scoped></style>
 <style lang='css' scoped>
-  #contact {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  #content {
-    width: 70vw;
-    height: 60vh;
-    margin-bottom: 5rem;
-    background-color: rgb(250, 250, 250);
-    border-radius: 1rem;
-  }
-  #cTitle {
-    height: 10vh;
-    /* background-color: pink; */
-    color: rgb(100, 100, 100);
-    font: 2rem/10vh "Microsoft YaHei";
-    text-align: center;
-  }
-  #cInfo {
-    height: 40vh;
-    /* background-color: skyblue; */
-    display: flex;
-    justify-content: center;
-  }
-  #cInfo>ul {
-    width: 32vw;
-    height: 100%;
-  }
-   #cInfo>ul>li {
-     display: flex;
-     align-items: center;
-     margin-bottom: 3rem;
-   }
-  .cIcon {
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    background: var(--rFontColorA) center/60% no-repeat;
-  }
-  .cIconText {
-    vertical-align: middle;
-    margin-left: 1rem;
-    font: 1.2rem sans-serif;
-  }
-  #cInfo>div {
-    width: 32vw;
-    height: 100%;
-  }
-  #cSend {
-    background-color: white;
-    border-radius: 0.5rem;
-    overflow: hidden;
-    box-sizing: border-box;
-    border: 1px solid rgba(0,0,0,0.05);
-  }
-  #cSend>div:first-child {
-    height: 3vh;
-    background-color: var(--rFontColor);
-    color: white;
-    font: 1.5rem/3vh  "Microsoft YaHei";
-    padding-left:1rem;
-    border-radius: 0.5rem;
-  }
-  #cOther {
-    box-sizing: border-box;
-    border-top: 1px solid rgba(0,0,0,0.1);
-    width: 64vw;
-    height: 10vh;
-    margin: 0 auto;
-    margin-top: 2rem;
-    /* background-color: olive !important; */
-  }
 </style>
